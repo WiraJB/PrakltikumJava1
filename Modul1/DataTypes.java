520b2f339a415008181ceb42f67a0cf7d72b64b3
WiraJB <wirajulian88@gmail.com>
2020-04-22 09:56:17 +0800

    Output dengan sout

--- Modul1/DataTypes.java
+++ Modul1/DataTypes.java
@@ -5,10 +5,19 @@ public class DataTypes{
 	String namaTengah = "Julian";
 	String namaBelakang = "Bayangkara";
 	int usia = 18;
-	int tergetTahunKuliah = 4;
+	int targetTahunKuliah = 4;
 	double ipk = 3.8795462;
 	char nilaiAbjad = 'A';
 	boolean tampan = true;
+	
+	System.out.println("Nama depan : " + namaDepan);
+	System.out.println("Nama tengah : " + namaTengah);
+	System.out.println("Nama belakang : " + namaBelakang);
+	System.out.println("Usia : " + usia);
+	System.out.println("Target Kuliah : " + targetTahunKuliah);
+	System.out.println("IPK : " + ipk);
+	System.out.println("Nilai PBO : " + nilaiAbjad);
+	System.out.println("Tampan : " + tampan);
 	
 	}
 }
