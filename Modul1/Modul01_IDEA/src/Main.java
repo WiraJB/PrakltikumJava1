8c631bdef1fb04e7c29a9c892b2e2ddd3325f25f
WiraJB <wirajulian88@gmail.com>
2020-04-22 16:43:45 +0800

    Project IDEA

---
+++ Modul1/Modul01_IDEA/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String namaDepan= "Wira";
+        String namaTengah = "Julian";
+        String namaBelakang = "Bayangkara";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.8795462;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
