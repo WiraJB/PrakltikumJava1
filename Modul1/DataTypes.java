8738480b330422e0343ce5cd625bc4ba8d44e090
WiraJB <wirajulian88@gmail.com>
2020-04-22 09:49:21 +0800

    deklarasi variabel dan tipe data

---
+++ Modul1/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+	public static void main(String[] args){
+	
+	String namaDepan= "Wira";
+	String namaTengah = "Julian";
+	String namaBelakang = "Bayangkara";
+	int usia = 18;
+	int tergetTahunKuliah = 4;
+	double ipk = 3.8795462;
+	char nilaiAbjad = 'A';
+	boolean tampan = true;
+	
+	}
+}
